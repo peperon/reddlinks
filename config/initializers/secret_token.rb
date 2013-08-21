921e7ff14071f3693809cdcba0fc3b249eec4e04
Peter Kostov <pekostov@gmail.com>
2013-08-21 21:45:41 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reddlinks::Application.config.secret_token = 'e4027cf93d7f9f8f741cb78ada9588206c075e01d39cd8c69ee6aa66f93c4352b6b9e9c3677a5d33a5c89d3c2a639de3788ab60c6a3de2d3ae2e3cb252c6fe4a'
