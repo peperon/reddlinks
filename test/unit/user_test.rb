922e82051b419e51c2eaa0d6cc0825abdf131d48
Peter Kostov <pekostov@gmail.com>
2013-08-30 23:53:09 +0300

    Unit test of user model

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "users attributes must not be empty" do
+    user = User.new
+
+    assert user.invalid?
+    assert user.errors[:name].any?
+    assert user.errors[:password_digest].any?
+  end
+
+  test "user name must be unique" do
+    user = User.new(name:            users(:one).name,
+                    password_digest: "Some random password")
+
+    assert !user.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'),
+                 user.errors[:name].join('; ')
+  end
 end
